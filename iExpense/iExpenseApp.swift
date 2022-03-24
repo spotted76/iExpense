8f1d01bead5ebac2e511b74f1f85f3be61b3e4b8
Peter Fischer <spotted@gmail.com>
2022-03-24 18:03:53 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Peter Fischer on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
